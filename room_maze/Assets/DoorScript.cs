eeb28af68ddb56818e44bd20d70eb42abdfdc86d
Chris Caragianis <chriscaragianis@gmail.com>
2019-01-24 22:19:55 -0500

    panel

---
+++ room_maze/Assets/DoorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+	private Animator _animator;
+
+	void Start() {
+		_animator = GetComponent<Animator> ();
+		_animator.SetBool ("open", false);
+	}
+
+	void OnTriggerEnter(Collider other) {	
+		if (other.CompareTag("Player")) {
+			_animator.SetBool ("open", true);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.CompareTag("Player"))	 {
+			_animator.SetBool ("open", false);
+		}
+	}
+}
