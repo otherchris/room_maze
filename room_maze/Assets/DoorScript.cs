3cdddf51757e542194cffd8fc2a1cc52832878ce
Chris Caragianis <chriscaragianis@gmail.com>
2019-01-24 23:00:35 -0500

    all stuff

--- room_maze/Assets/DoorScript.cs
+++ room_maze/Assets/DoorScript.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour {
 
 	private Animator _animator;
+	private AudioSource[] sounds;
 
 	void Start() {
+		sounds = GetComponents<AudioSource> ();
 		_animator = GetComponent<Animator> ();
 		_animator.SetBool ("open", false);
 	}
@@ -14,12 +16,14 @@ public class DoorScript : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {	
 		if (other.CompareTag("Player")) {
 			_animator.SetBool ("open", true);
+			sounds [0].Play ();
 		}
 	}
 
 	void OnTriggerExit(Collider other) {
 		if (other.CompareTag("Player"))	 {
 			_animator.SetBool ("open", false);
+			sounds [1].Play ();
 		}
 	}
 }
